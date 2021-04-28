6e62e299c32185272190611f2cf7baf2316d7edc
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2021-04-29 01:55:11 +0700

    Add user entities

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class User {}
+import { Entity, ObjectID, ObjectIdColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @ObjectIdColumn() id: ObjectID;
+  @Column() email: string;
+  @Column() password: string;
+
+  constructor(user?: Partial<User>) {
+    Object.assign(this, user);
+  }
+}
