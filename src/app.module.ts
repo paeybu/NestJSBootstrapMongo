33c2c82bd50391f4833ede733a9d08cf7b32ff40
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2021-04-29 01:52:25 +0700

    Generate users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       useUnifiedTopology: true,
       useNewUrlParser: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
